6380bc8a4a7413f96d24783732e355b6aec586df
kelxy14 <kelsey_wright@my.uri.edu>
2014-06-19 14:53:39 -0400

    Add script to parse GapFind output
    The script will print no-production metabolites and additional data on
    these metabolites from a compound table.

---
+++ parse_gapfind_noproduction.py
@@ -0,0 +1,49 @@
+
+'''Parse GapFind output and print no-production metabolites'''
+
+import sys
+import csv 
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'USAGE: {} GAPFIND CPDLIST'.format(sys.argv[0])
+        sys.exit(-1)
+
+    f = open(sys.argv[1], 'r')
+    c = open(sys.argv[2], 'r')
+
+    compound_map = {}
+
+    readerc = csv.reader(c, dialect='excel')
+    for rowc in readerc:
+        SEED_cid, Formula, Mass, KEGG_cid, CPD_name = rowc[:5]
+        compound_map[SEED_cid] = Formula, KEGG_cid, CPD_name
+
+    count = 0
+    count_not_produced = 0
+    count_in_kegg = 0
+    flag = False
+    for line in f:
+        if line.startswith('---- VAR xp'):
+            flag = True
+            for i in range(3):
+                next(f)
+        elif flag:
+            if line == '\n':
+                break
+
+            fields = line.split()
+            cpdid = fields[0]
+            produced = fields[2] != '.'
+            count += 1
+
+            if not produced:
+                if not cpdid.endswith('_e'):
+                    Formula, KEGG_cid, CPD_name = compound_map[cpdid]
+                    count_not_produced += 1
+                    if KEGG_cid != "None":
+                        count_in_kegg += 1
+                    print '{}\t{}\t{}'.format(cpdid, KEGG_cid, Formula)
+
+    print 'Not produced: {}/{}'.format(count_not_produced, count)
+    print 'In KEGG: {}/{}'.format(count_in_kegg, count_not_produced)
