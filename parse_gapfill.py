f1a63b30c025a3c26dab00110e043324ec3b90b7
kelxy14 <kelsey_wright@my.uri.edu>
2014-06-26 13:28:01 -0400

    Print out information on transport and exchange reactions

--- parse_gapfill.py
+++ parse_gapfill.py
@@ -12,9 +12,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse GapFill output')
     parser.add_argument('gapfill', type=argparse.FileType('r'), help='GapFill output file (ps=0)')
     parser.add_argument('rxnfile', type=argparse.FileType('r'), help='Reaction table file')
+    parser.add_argument('compoundfile', type=argparse.FileType('r'), help='Compound Map file')
+    
     args = parser.parse_args()
 
     reaction_map = {}
+    compound_map = {}
 
     rxnfile = args.rxnfile
     rxnfile.readline() # Skip header
@@ -22,6 +25,14 @@ if __name__ == '__main__':
         seed_rid, name, equation = row[:3]
         reaction_map[seed_rid] = name, equation
     rxnfile.close()
+
+    compoundfile = args.compoundfile
+    compoundfile.readline() # Skip header
+    for row in csv.reader(compoundfile, dialect='excel'):
+        SEED_cid, Formula, Mass, KEGG_cid, cpd_names = row
+        main_name = cpd_names.split(',<br>')[0]
+        compound_map[SEED_cid] = Formula, main_name
+    compoundfile.close()
 
     # Parse GapFind output
     count = 0
@@ -46,8 +57,14 @@ if __name__ == '__main__':
                 if rxn_id in reaction_map:
                     name, equation = reaction_map[rxn_id]
                     print '{}\t{}\t{}'.format(rxn_id, name, equation)
-                else:
-                    print rxn_id
+                elif rxn_id.startswith('rxntp_'):
+                    cpdid = rxn_id[6:14]
+                    formula, cpdname = compound_map[cpdid]
+                    print '{}\t{}\t{}'.format(rxn_id, 'Transport of ' + cpdname, '|{}[e]| => |{}|'.format(cpdname, cpdname))
+                elif rxn_id.startswith('rxnex_'):
+                    cpdid = rxn_id[6:14]
+                    formula, cpdname = compound_map[cpdid]
+                    print '{}\t{}\t{}'.format(rxn_id, 'Exchange of ' + cpdname, '|{}[e]| <=>'.format(cpdname, cpdname))
             
     gffile.close()
 
