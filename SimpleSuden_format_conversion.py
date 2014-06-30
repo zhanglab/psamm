83db4a7a63aaa2f7a348087464a2bfca161212c9
kelxy14 <kelsey_wright@my.uri.edu>
2014-06-30 11:01:20 -0400

    Changes SimpleSuden rxns to standard format

---
+++ SimpleSuden_format_conversion.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import csv
+import argparse
+import reaction
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Convert Suden reaction table to standard format')
+    parser.add_argument('rxnfile', type=argparse.FileType('r'), help='Reaction table file')
+    
+    args = parser.parse_args()
+
+    rxn_table = args.rxnfile
+    
+    rxn_table.readline() # Skip header
+    for row in csv.reader(rxn_table, delimiter='\t'):
+        SEED_rid, rxn_name, equation_cpd, equation_name, Subsystem, Suden_protien_ID, gene_association, protein_id, ec_reference, Reversibility = row[:10]
+        rx = reaction.SudenSimple.parse(equation_cpd).normalized()
+        
+        print '{}\t{}'.format(SEED_rid, rx)
+    
+    rxn_table.close()
+    
