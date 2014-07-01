dd850280363f0e988d075f8b03271e942aca8173
kelxy14 <kelsey_wright@my.uri.edu>
2014-07-01 15:35:14 -0400

    Fixed reversibility of SimpleSuden rxns

--- SimpleSuden_format_conversion.py
+++ SimpleSuden_format_conversion.py
@@ -15,13 +15,13 @@ if __name__ == '__main__':
     
     rxn_table.readline() # Skip header
     for row in csv.reader(rxn_table, delimiter='\t'):
-        SEED_rid, rxn_name, equation_cpd, equation_name, Subsystem, Suden_protien_ID, gene_association, protein_id, ec_reference, Reversibility = row[:10]
+        SEED_rid, rxn_name, equation_cpd, equation_name, Subsystem, Suden_protien_ID, gene_association, protein_id, ec_num, reference, Reversibility = row[:11]
         
         rx = reaction.SudenSimple.parse(equation_cpd).normalized()
         if Reversibility == 'N':
             rx.direction = '=>'
         SEED_rid = SEED_rid.lower()
-        
+
         print '{}\t{}'.format(SEED_rid, rx)
     
     rxn_table.close()
