779b060e1a9e0e589b94a7e7d0dd779764411369
kelxy14 <kelsey_wright@my.uri.edu>
2014-07-15 11:02:26 -0400

    Corrected SimpleSuden for uppercase letters

--- SimpleSuden_format_conversion.py
+++ SimpleSuden_format_conversion.py
@@ -20,8 +20,12 @@ if __name__ == '__main__':
         rx = reaction.SudenSimple.parse(equation_cpd).normalized()
         if Reversibility == 'N':
             rx.direction = '=>'
-        SEED_rid = SEED_rid.lower()
 
+        if not SEED_rid.islower() and SEED_rid[:3].lower() == 'rxn':
+            SEED_rid = 'simplesuden_' + SEED_rid.lower()
+        else:
+            SEED_rid = SEED_rid.lower()
+        
         print '{}\t{}'.format(SEED_rid, rx)
     
     rxn_table.close()
