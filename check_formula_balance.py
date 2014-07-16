e9684d704866588ebcdfc08f17da23d771568f8e
Jon Lund Steffensen <jonlst@gmail.com>
2014-07-16 11:49:52 -0400

    formula: Change formula representation to specialized class Formula
    This new class supports addition, multiplication by scalar and
    equality/inequality comparison of objects. The parse method is
    implemented as a classmethod on this class.

--- check_formula_balance.py
+++ check_formula_balance.py
@@ -1,59 +1,46 @@
 #!/usr/bin/env python
 
 import csv
-import formula
+from formula import Formula
 import reaction
 import argparse
 import pprint
 
 # Main program
 if __name__ == '__main__':
-    # sys.argv[1] contains the file name to open
-    
     parser = argparse.ArgumentParser(description='Convert reaction table to GapFind input format')
     parser.add_argument('rxnfile', type=argparse.FileType('r'), help='Reaction table file')
     parser.add_argument('cpdfile', type=argparse.FileType('r'), help='Compound table file')
-    
+
     args = parser.parse_args()
 
     rxn_file = args.rxnfile
     cpd_file = args.cpdfile
-    
-    compound_map = {}   #empty dictionary
-    
+
+    # Mapping from compound id to formula
+    compound_formula = {}
+
     readerc = csv.reader(cpd_file,delimiter='\t')
     for rowc in readerc:
-        SEED_cid, cpd_names, Formula, Mass,KEGG_maps, KEGG_cid = rowc  
+        SEED_cid, cpd_names, formula, Mass,KEGG_maps, KEGG_cid = rowc
 
-        compound_map[SEED_cid] = Formula
-        #setting the output of SEED_cid equal to Formula   
-    
+        compound_formula[SEED_cid] = Formula.parse(formula)
+
     readerr = csv.reader(rxn_file,delimiter='\t')
     for rowr in readerr:
         rxn_id,Equation_cpdid = rowr[:2]
-        
+
         if Equation_cpdid.strip() == '':
             continue
 
         rx = reaction.ModelSEED.parse(Equation_cpdid).normalized()
-        
-        left_form = {} #empty dictionary
-        for cpdid, count, comp in rx.left:
-            form = compound_map[cpdid]
-            f = formula.parse(form)
-            left_form = formula.sum(left_form, formula.multiply(f, count))
-        #print left_form
-            
-        right_form = {} #empty dictionary
-        for cpdid, count, comp in rx.right:
-            form = compound_map[cpdid]
-            f = formula.parse(form)
-            right_form = formula.sum(right_form, formula.multiply(f, count))   
-        #print right_form    
-            
+
+        left_form = sum((count * compound_formula[cpdid] for cpdid, count, comp in rx.left), Formula())
+        right_form = sum((count * compound_formula[cpdid] for cpdid, count, comp in rx.right), Formula())
+
         if right_form != left_form:
                 print '{}\t{}\t{}'.format(rxn_id, pprint.pformat(left_form),
-                                            pprint.pformat(right_form))        
-            
+                                            pprint.pformat(right_form))
+
     rxn_file.close()
-    cpd_file.close()  
+    cpd_file.close()
--- formula.py
+++ formula.py
@@ -1,72 +1,113 @@
 
 '''Parser for chemical formulas'''
 
-def parse(s):
-    '''Parse a formula string (e.g. C6H10O2)
+import pprint
+from collections import defaultdict
 
-    >>> import pprint
-    >>> pprint.pprint(parse_formula('H2O2'))
-    {'H': 2, 'O': 2}
-    >>> pprint.pprint(parse_formula('H2O'))
-    {'H': 2, 'O': 1}
-    >>> pprint.pprint(parse_formula('Zn'))
-    {'Zn': 1}
-    >>> pprint.pprint(parse_formula('C2H5NO2'))
-    {'C': 2, 'H': 5, 'N': 1, 'O': 2}
-    '''
+class Formula(object):
+    def __init__(self, values={}):
+        self._values = dict(values)
 
-    formula = {}
-    looking_for_number = True
-    has_read_uppercase = False
+    def __repr__(self):
+        return 'Formula({})'.format(pprint.pformat(self._values))
 
-    number = ''
-    atom = ''
-    
-    for i in range(len(s)):
-        if looking_for_number:
-            if s[i].isdigit():
-                looking_for_number = False
-                number += s[i]
-            elif s[i].islower():
-                atom += s[i]
+    def __add__(self, other):
+        '''Sum of self and other
+
+        >>> Formula({'H': 2, 'O': 1}) + Formula({'N': 1, 'O': 2})
+        Formula({'H': 2, 'N': 1, 'O': 3})
+        '''
+        result = defaultdict(int)
+        for f in (self._values, other._values):
+            for key, value in f.items():
+                result[key] += value
+        return Formula(result)
+
+    def __mul__(self, other):
+        '''Multiply each count by other
+
+        >>> Formula({'H': 2, 'O': 1}) * 4
+        Formula({'H': 8, 'O': 4})'''
+
+        return Formula({ key: value*other for key, value in self._values.items() })
+
+    def __rmul__(self, other):
+        '''Multiply each count by other
+
+        >>> 2 * Formula({'H': 2, 'O': 1})
+        Formula({'H': 4, 'O': 2})'''
+
+        return self * other
+
+    def __eq__(self, other):
+        '''Equality of self and other
+
+        >>> Formula({'H': 2, 'O': 1}) == Formula({'O': 1, 'H': 2})
+        True
+        >>> Formula({'Au': 1}) == Formula({'Ag': 1})
+        False'''
+
+        return self._values == other._values
+
+    def __ne__(self, other):
+        '''Not equality of self and other
+
+        >>> Formula({'Au': 1}) != Formula({'Ag': 1})
+        True'''
+
+        return self._values != other._values
+
+    @classmethod
+    def parse(cls, s):
+        '''Parse a formula string (e.g. C6H10O2)
+
+        >>> Formula.parse('H2O2')
+        Formula({'H': 2, 'O': 2})
+        >>> Formula.parse('H2O')
+        Formula({'H': 2, 'O': 1})
+        >>> Formula.parse('Zn')
+        Formula({'Zn': 1})
+        >>> Formula.parse('C2H5NO2')
+        Formula({'C': 2, 'H': 5, 'N': 1, 'O': 2})
+        '''
+
+        formula = {}
+        looking_for_number = True
+        has_read_uppercase = False
+
+        number = ''
+        atom = ''
+
+        for i in range(len(s)):
+            if looking_for_number:
+                if s[i].isdigit():
+                    looking_for_number = False
+                    number += s[i]
+                elif s[i].islower():
+                    atom += s[i]
+                else:
+                    if has_read_uppercase:
+                        # Implicit 1
+                        formula[atom] = 1
+                        atom = s[i]
+                        number = ''
+                    else:
+                        atom += s[i]
+                        has_read_uppercase = True
             else:
-                if has_read_uppercase:
-                    # Implicit 1
-                    formula[atom] = 1
+                if s[i].isdigit():
+                    number += s[i]
+                else:
+                    formula[atom] = int(number)
                     atom = s[i]
                     number = ''
-                else:
-                    atom += s[i]
+                    looking_for_number = True
                     has_read_uppercase = True
-        else:
-            if s[i].isdigit():
-                number += s[i]
-            else:
-                formula[atom] = int(number)
-                atom = s[i]
-                number = ''
-                looking_for_number = True
-                has_read_uppercase = True
 
-    formula[atom] = int(number) if number != '' else 1
+        formula[atom] = int(number) if number != '' else 1
+        return Formula(formula)
 
-    return formula
-
-def multiply(formula, n):
-    return { key: value*n for key, value in formula.items() }
-
-def sum(f1, f2):
-    '''Return sum of two formulas'''
-    formula = {}
-    for f in (f1, f2):
-        for key, value in f.items():
-            if key in formula:
-                formula[key] += value
-            else:
-                formula[key] = value
-    return formula
 
 if __name__ == '__main__':
-
     import doctest
     doctest.testmod()
