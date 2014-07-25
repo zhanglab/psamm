7124b8f1be22b56be56f826cdaf04ccae316cccc
Jon Lund Steffensen <jonlst@gmail.com>
2014-07-25 18:47:26 -0400

    fastcore: Use cplex python API directly instead of using PuLP

--- fastcore.py
+++ fastcore.py
@@ -8,6 +8,7 @@ network models." PLoS computational biology 10.1 (2014):
 e1003424.'''
 
 import pulp
+import cplex
 from itertools import izip
 
 def fastcore_lp3(model, reaction_subset):
@@ -47,8 +48,42 @@ def fastcore_lp3(model, reaction_subset):
         raise Exception('Non-optimal solution: {}'.format(pulp.LpStatus[status]))
 
     for rxnid, flux in izip(reaction_list, fluxes):
-        #print '{}\t{}'.format(rxnid, flux.value())
         yield rxnid, flux.value()
+
+def fastcore_lp3_cplex(model, reaction_subset):
+    # Create LP-3 problem of Fastcore
+    prob = cplex.Cplex()
+
+    # Define flux variables
+    flux_names = []
+    flux_lower = []
+    flux_upper = []
+    flux_obj = []
+    for rxnid in model.reaction_set:
+        lower, upper = model.limits[rxnid]
+        flux_names.append('v_'+rxnid)
+        flux_lower.append(lower)
+        flux_upper.append(upper)
+        flux_obj.append(1 if rxnid in reaction_subset else 0)
+    prob.variables.add(names=flux_names, lb=flux_lower, ub=flux_upper, obj=flux_obj)
+
+    # Define constraints
+    massbalance_lhs = { cpdid: [] for cpdid in model.compound_set }
+    for spec, value in model.matrix.iteritems():
+        cpdid, rxnid = spec
+        massbalance_lhs[cpdid].append(('v_'+rxnid, value))
+    for cpdid, lhs in massbalance_lhs.iteritems():
+        prob.linear_constraints.add(lin_expr=[cplex.SparsePair(*zip(*lhs))], senses=['E'], rhs=[0], names=['massbalance_'+cpdid])
+
+    # Solve
+    prob.objective.set_sense(prob.objective.sense.maximize)
+    prob.solve()
+    status = prob.solution.get_status()
+    if status != 1:
+        raise Exception('Non-optimal solution: {}'.format(prob.solution.get_status_string()))
+
+    for rxnid in model.reaction_set:
+        yield rxnid, prob.solution.get_values('v_'+rxnid)
 
 def fastcore_lp7(model, reaction_subset, epsilon):
     # Create LP-7 problem of Fastcore
@@ -96,8 +131,49 @@ def fastcore_lp7(model, reaction_subset, epsilon):
         raise Exception('Non-optimal solution: {}'.format(pulp.LpStatus[status]))
 
     for rxnid, flux in izip(reaction_list, fluxes):
-        #print '{}\t{}'.format(rxnid, flux.value())
         yield rxnid, flux.value()
+
+def fastcore_lp7_cplex(model, reaction_subset, epsilon):
+    # Create LP-7 problem of Fastcore
+    prob = cplex.Cplex()
+
+    # Define flux variables
+    flux_names = []
+    flux_lower = []
+    flux_upper = []
+    for rxnid in model.reaction_set:
+        lower, upper = model.limits[rxnid]
+        flux_names.append('v_'+rxnid)
+        flux_lower.append(lower)
+        flux_upper.append(upper)
+    prob.variables.add(names=flux_names, lb=flux_lower, ub=flux_upper)
+
+    # Define z variables
+    zs_names = []
+    for rxnid in reaction_subset:
+        zs_names.append('z_'+rxnid)
+    prob.variables.add(names=zs_names, lb=[0]*len(zs_names), ub=[epsilon]*len(zs_names), obj=[1]*len(zs_names))
+
+    # Define constraints
+    for rxnid in reaction_subset:
+        prob.linear_constraints.add(lin_expr=[cplex.SparsePair(ind=('v_'+rxnid, 'z_'+rxnid), val=(1, -1))], senses=['G'], rhs=[0])
+
+    massbalance_lhs = { cpdid: [] for cpdid in model.compound_set }
+    for spec, value in model.matrix.iteritems():
+        cpdid, rxnid = spec
+        massbalance_lhs[cpdid].append(('v_'+rxnid, value))
+    for cpdid, lhs in massbalance_lhs.iteritems():
+        prob.linear_constraints.add(lin_expr=[cplex.SparsePair(*zip(*lhs))], senses=['E'], rhs=[0], names=['massbalance_'+cpdid])
+
+    # Solve
+    prob.objective.set_sense(prob.objective.sense.maximize)
+    prob.solve()
+    status = prob.solution.get_status()
+    if status != 1:
+        raise Exception('Non-optimal solution: {}'.format(prob.solution.get_status_string()))
+
+    for rxnid in model.reaction_set:
+        yield rxnid, prob.solution.get_values('v_'+rxnid)
 
 def fastcore_lp10(model, subset_k, subset_p, epsilon):
     if len(subset_k) == 0:
@@ -161,6 +237,62 @@ def fastcore_lp10(model, subset_k, subset_p, epsilon):
     for rxnid, flux in izip(reaction_list, fluxes):
         yield rxnid, flux.value()
 
+def fastcore_lp10_cplex(model, subset_k, subset_p, epsilon):
+    if len(subset_k) == 0:
+        return
+
+    scaling = 1e5
+
+    # Create LP-10 problem of Fastcore
+    prob = cplex.Cplex()
+
+    # Define flux variables
+    flux_names = []
+    flux_lower = []
+    flux_upper = []
+    max_bound = 0
+    for rxnid in model.reaction_set:
+        lower, upper = model.limits[rxnid]
+        for b in (lower, upper):
+            if abs(b) >= max_bound:
+                max_bound = abs(b)
+        flux_names.append('v_'+rxnid)
+        flux_lower.append(lower*scaling)
+        flux_upper.append(upper*scaling)
+    prob.variables.add(names=flux_names, lb=flux_lower, ub=flux_upper)
+
+    # Define z variables
+    zs_names = []
+    for rxnid in subset_p:
+        zs_names.append('z_'+rxnid)
+    prob.variables.add(names=zs_names, lb=[0]*len(zs_names), ub=[max_bound*scaling]*len(zs_names), obj=[1]*len(zs_names))
+
+    # Define constraints
+    for rxnid in subset_p:
+        prob.linear_constraints.add(lin_expr=[cplex.SparsePair(ind=('v_'+rxnid, 'z_'+rxnid), val=(1, 1)), # v >= -z
+                                                cplex.SparsePair(ind=('v_'+rxnid, 'z_'+rxnid), val=(-1, 1))], # z >= v
+                                    senses=['G', 'G'], rhs=[0, 0])
+
+    prob.linear_constraints.add(lin_expr=[cplex.SparsePair(ind=['v_'+rxnid], val=[1]) for rxnid in subset_k],
+                                senses=['G'] * len(subset_k), rhs=[epsilon*scaling] * len(subset_k))
+
+    massbalance_lhs = { cpdid: [] for cpdid in model.compound_set }
+    for spec, value in model.matrix.iteritems():
+        cpdid, rxnid = spec
+        massbalance_lhs[cpdid].append(('v_'+rxnid, value))
+    for cpdid, lhs in massbalance_lhs.iteritems():
+        prob.linear_constraints.add(lin_expr=[cplex.SparsePair(*zip(*lhs))], senses=['E'], rhs=[0], names=['massbalance_'+cpdid])
+
+    # Solve
+    prob.objective.set_sense(prob.objective.sense.minimize)
+    prob.solve()
+    status = prob.solution.get_status()
+    if status != 1:
+        raise Exception('Non-optimal solution: {}'.format(prob.solution.get_status_string()))
+
+    for rxnid in model.reaction_set:
+        yield rxnid, prob.solution.get_values('v_'+rxnid)
+
 def support_set(fluxiter, threshold):
     '''Return the support set of the fluxes
 
@@ -180,13 +312,13 @@ def fastcc(model, epsilon):
     consistent_subset = set()
 
     subset = model.reaction_set - model.reversible
-    print '|J| = {}'.format(len(subset))
-    print 'J = {}'.format(subset)
+    #print '|J| = {}'.format(len(subset))
+    #print 'J = {}'.format(subset)
 
-    support = support_set(fastcore_lp7(model, subset, epsilon), 0.99*epsilon)
+    support = support_set(fastcore_lp7_cplex(model, subset, epsilon), 0.99*epsilon)
     consistent_subset |= support
-    print '|A| = {}'.format(len(consistent_subset))
-    print 'A = {}'.format(consistent_subset)
+    #print '|A| = {}'.format(len(consistent_subset))
+    #print 'A = {}'.format(consistent_subset)
 
     inconsistent_subset = subset - support
 
@@ -194,28 +326,28 @@ def fastcc(model, epsilon):
         print 'Subset inconsistent: {}'.format(inconsistent_subset)
 
     subset = (model.reaction_set - support) - inconsistent_subset
-    print '|J| = {}'.format(len(subset))
-    print 'J = {}'.format(subset)
+    #print '|J| = {}'.format(len(subset))
+    #print 'J = {}'.format(subset)
 
     flipped = False
     singleton = False
     while len(subset) > 0:
         if singleton:
             subset_i = set((next(iter(subset)),))
-            print 'LP3 on {}'.format(subset_i)
-            support = support_set(fastcore_lp3(model, subset_i), 0.99*epsilon)
+            #print 'LP3 on {}'.format(subset_i)
+            support = support_set(fastcore_lp3_cplex(model, subset_i), 0.99*epsilon)
         else:
             subset_i = subset
-            print 'LP7 on {}'.format(subset_i)
-            support = support_set(fastcore_lp7(model, subset_i, epsilon), 0.99*epsilon)
+            #print 'LP7 on {}'.format(subset_i)
+            support = support_set(fastcore_lp7_cplex(model, subset_i, epsilon), 0.99*epsilon)
         consistent_subset |= support
-        print '|A| = {}'.format(len(consistent_subset))
-        print 'A = {}'.format(consistent_subset)
+        #print '|A| = {}'.format(len(consistent_subset))
+        #print 'A = {}'.format(consistent_subset)
 
         if not subset.isdisjoint(consistent_subset):
             subset -= consistent_subset
-            print '|J| = {}'.format(len(subset))
-            print 'J = {}'.format(subset)
+            #print '|J| = {}'.format(len(subset))
+            #print 'J = {}'.format(subset)
             flipped = False
         else:
             subset_rev_i = subset_i & model.reversible
@@ -229,7 +361,7 @@ def fastcc(model, epsilon):
             else:
                 model = model.flipped(subset_rev_i)
                 flipped = True
-                print 'Flip'
+                #print 'Flip'
 
     return consistent_subset
 
@@ -241,21 +373,21 @@ def find_sparse_mode(model, core, additional, singleton, epsilon):
 
     if singleton:
         subset_i = set((next(iter(core)),))
-        print 'LP7 on {}'.format(subset_i)
-        support = support_set(fastcore_lp7(model, subset_i, epsilon), 0.99*epsilon)
+        #print 'LP7 on {}'.format(subset_i)
+        support = support_set(fastcore_lp7_cplex(model, subset_i, epsilon), 0.99*epsilon)
     else:
-        print 'LP7 on {}'.format(core)
-        support = support_set(fastcore_lp7(model, core, epsilon), 0.99*epsilon)
+        #print 'LP7 on {}'.format(core)
+        support = support_set(fastcore_lp7_cplex(model, core, epsilon), 0.99*epsilon)
 
-    print 'Support = {}'.format(support)
+    #print 'Support = {}'.format(support)
     k = core & support
 
     if len(k) == 0:
         return set()
 
-    print 'K = {}'.format(k)
-    print 'LP10 on K={} and P={}'.format(k, additional)
-    return support_set(fastcore_lp10(model, k, additional, epsilon), 0.99*epsilon)
+    #print 'K = {}'.format(k)
+    #print 'LP10 on K={} and P={}'.format(k, additional)
+    return support_set(fastcore_lp10_cplex(model, k, additional, epsilon), 0.99*epsilon)
 
 def fastcore(model, core, epsilon):
     '''Find a flux consistent subnetwork containing the core subset
@@ -266,20 +398,20 @@ def fastcore(model, core, epsilon):
     consistent_subset = set()
 
     subset = core - model.reversible
-    print '|J| = {}, J = {}'.format(len(subset), subset)
+    #print '|J| = {}, J = {}'.format(len(subset), subset)
 
     penalty_set = model.reaction_set - core
-    print '|P| = {}, P = {}'.format(len(penalty_set), penalty_set)
+    #print '|P| = {}, P = {}'.format(len(penalty_set), penalty_set)
 
     mode = find_sparse_mode(model, subset, penalty_set, False, epsilon)
     if len(subset - mode) > 0:
         raise Exception('Inconsistent irreversible core reactions')
 
     consistent_subset |= mode
-    print '|A| = {}, A = {}'.format(len(consistent_subset), consistent_subset)
+    #print '|A| = {}, A = {}'.format(len(consistent_subset), consistent_subset)
 
     subset = core - mode
-    print '|J| = {}, J = {}'.format(len(subset), subset)
+    #print '|J| = {}, J = {}'.format(len(subset), subset)
 
     flipped = False
     singleton = False
@@ -287,11 +419,11 @@ def fastcore(model, core, epsilon):
         penalty_set -= consistent_subset
         mode = find_sparse_mode(model, subset, penalty_set, singleton, epsilon)
         consistent_subset |= mode
-        print '|A| = {}, A = {}'.format(len(consistent_subset), consistent_subset)
+        #print '|A| = {}, A = {}'.format(len(consistent_subset), consistent_subset)
 
         if not subset.isdisjoint(consistent_subset):
             subset -= consistent_subset
-            print '|J| = {}, J = {}'.format(len(subset), subset)
+            #print '|J| = {}, J = {}'.format(len(subset), subset)
             flipped = False
         else:
             if singleton:
@@ -309,6 +441,6 @@ def fastcore(model, core, epsilon):
             else:
                 model = model.flipped(subset_rev_i)
                 flipped = True
-                print 'Flip'
+                #print 'Flip'
 
     return consistent_subset
