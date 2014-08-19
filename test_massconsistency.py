3e1bb1f4689ea0fbc7422ba6710d12e42dfa5ab7
Jon Lund Steffensen <jonlst@gmail.com>
2014-08-19 15:21:10 -0400

    Add unit tests for massconsistency

---
+++ test_massconsistency.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import unittest
+
+import metabolicmodel
+import massconsistency
+import lpsolver
+from reaction import ModelSEED
+
+class TestMassConsistency(unittest.TestCase):
+    '''Test fastcore using a simple model'''
+
+    def setUp(self):
+        # TODO use mock model instead of actual model
+        self.database = metabolicmodel.MetabolicDatabase()
+        self.database.set_reaction('rxn_1', ModelSEED.parse('=> (2) |A|'))
+        self.database.set_reaction('rxn_2', ModelSEED.parse('|A| <=> |B|'))
+        self.database.set_reaction('rxn_3', ModelSEED.parse('|A| => |D|'))
+        self.database.set_reaction('rxn_4', ModelSEED.parse('|A| => |C|'))
+        self.database.set_reaction('rxn_5', ModelSEED.parse('|C| => |D|'))
+        self.database.set_reaction('rxn_6', ModelSEED.parse('|D| =>'))
+        self.model = self.database.load_model_from_file(iter(self.database.reactions))
+
+        self.masscons = massconsistency.MassConsistencyCheck(lpsolver.CplexSolver(None))
+
+    def test_mass_consistent_is_consistent(self):
+        exchange = { 'rxn_1', 'rxn_6' }
+        self.assertTrue(self.masscons.is_consistent(self.model, exchange, set()))
+
+    def test_mass_inconsistent_is_consistent(self):
+        exchange = { 'rxn_1', 'rxn_6' }
+        self.database.set_reaction('rxn_7', ModelSEED.parse('|D| => (2) |C|'))
+        self.model.add_reaction('rxn_7')
+        self.assertFalse(self.masscons.is_consistent(self.model, exchange, set()))
+
+if __name__ == '__main__':
+    unittest.main()
