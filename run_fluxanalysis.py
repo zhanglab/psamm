bcb7207dc7ecb54475994bb67a3c55b67365982e
Jon Lund Steffensen <jonlst@gmail.com>
2014-08-20 19:02:52 -0400

    Add script to run FBA and flux minimization

---
+++ run_fluxanalysis.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import csv
+
+from metabolicmodel import MetabolicDatabase
+import fluxanalysis
+import lpsolver
+
+if __name__ == '__main__':
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Run flux analysis on a metabolic model')
+    parser.add_argument('--database', required=True, metavar='reactionfile', action='append',
+                        type=argparse.FileType('r'), default=[],
+                        help='Reaction definition list to usa as database')
+    parser.add_argument('--compounds', metavar='compoundfile', action='append',
+                        type=argparse.FileType('r'), default=[],
+                        help='Optional compound information table')
+    parser.add_argument('reactionlist', type=argparse.FileType('r'), help='Model definition')
+    parser.add_argument('reaction', help='Reaction to maximize')
+    args = parser.parse_args()
+
+    database = MetabolicDatabase.load_from_files(*args.database)
+    model = database.load_model_from_file(args.reactionlist)
+
+    # Load compound information
+    compounds = {}
+    for compound_table in args.compounds:
+        compound_table.readline() # Skip header
+        for row in csv.reader(compound_table, delimiter='\t'):
+            cpdid, names = row[:2]
+            synonyms = names.split(',<br>')
+            name = synonyms.pop()
+            compounds[cpdid] = name
+
+    reaction = args.reaction
+    if reaction not in model.reaction_set:
+        sys.stderr.write('Specified reaction is not in model: {}\n'.format(reaction))
+        sys.exit(-1)
+
+    model.load_exchange_limits()
+
+    # Run FBA on model
+    fba_fluxes = dict(fluxanalysis.flux_balance(model, reaction))
+    optimum = fba_fluxes[reaction]
+    for rxnid, flux in sorted(fba_fluxes.iteritems()):
+        rx = database.get_reaction(rxnid)
+        print '{}\t{}\t{}'.format(rxnid, flux, rx.translated_compounds(lambda x: compounds.get(x, x)))
+    print 'Maximum: {}'.format(optimum)
+
+    epsilon = 1e-5
+
+    # Run flux minimization
+    fmin_fluxes = dict(fluxanalysis.flux_minimization(model, { reaction: optimum }))
+    count = 0
+    for rxnid, flux in sorted(fmin_fluxes.iteritems()):
+        if fba_fluxes[rxnid] - epsilon > flux:
+            count += 1
+        rx = database.get_reaction(rxnid)
+        print '{}\t{}\t{}\t{}'.format(rxnid, fba_fluxes[rxnid], flux, rx.translated_compounds(lambda x: compounds.get(x, x)))
+    print 'Minimized reactions: {}'.format(count)
