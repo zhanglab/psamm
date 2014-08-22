0f1b3491f8f3a45844a61e9f97f60ad79132e5d7
Jon Lund Steffensen <jonlst@gmail.com>
2014-08-22 13:57:36 -0400

    Add test.py script to run unit tests

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import unittest
+
+if __name__ == '__main__':
+    # Run unit tests in tests directory
+    testsuite = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(testsuite)
