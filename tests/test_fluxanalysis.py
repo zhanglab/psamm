f187066dccae2efa9a14d3fb541ef2204906c4c0
Jon Lund Steffensen <jonlst@gmail.com>
2014-08-25 14:17:23 -0400

    Add simple tests of fluxanalysis module

---
+++ tests/test_fluxanalysis.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import unittest
+
+from metnet import metabolicmodel
+from metnet import fluxanalysis
+from metnet import lpsolver
+from metnet.reaction import ModelSEED
+
+class TestFluxBalance(unittest.TestCase):
+    def setUp(self):
+        self.database = metabolicmodel.MetabolicDatabase()
+        self.database.set_reaction('rxn_1', ModelSEED.parse('=> (2) |A|'))
+        self.database.set_reaction('rxn_2', ModelSEED.parse('|A| <=> |B|'))
+        self.database.set_reaction('rxn_3', ModelSEED.parse('|A| => |D|'))
+        self.database.set_reaction('rxn_4', ModelSEED.parse('|A| => |C|'))
+        self.database.set_reaction('rxn_5', ModelSEED.parse('|C| => |D|'))
+        self.database.set_reaction('rxn_6', ModelSEED.parse('|D| =>'))
+        self.model = self.database.load_model_from_file(iter(self.database.reactions))
+        self.solver = lpsolver.CplexSolver(None)
+
+    def test_flux_balance_rxn_1(self):
+        fluxes = dict(fluxanalysis.flux_balance(self.model, 'rxn_1', solver=self.solver))
+        self.assertEqual(fluxes['rxn_1'], 500)
+        self.assertEqual(fluxes['rxn_2'], 0)
+        self.assertEqual(fluxes['rxn_6'], 1000)
+
+    def test_flux_balance_rxn_2(self):
+        fluxes = dict(fluxanalysis.flux_balance(self.model, 'rxn_2', solver=self.solver))
+        self.assertEqual(fluxes['rxn_2'], 0)
+
+    def test_flux_balance_rxn_3(self):
+        fluxes = dict(fluxanalysis.flux_balance(self.model, 'rxn_3', solver=self.solver))
+        self.assertEqual(fluxes['rxn_1'], 500)
+        self.assertEqual(fluxes['rxn_2'], 0)
+        self.assertEqual(fluxes['rxn_3'], 1000)
+        self.assertEqual(fluxes['rxn_6'], 1000)
+
+    def test_flux_balance_rxn_6(self):
+        fluxes = dict(fluxanalysis.flux_balance(self.model, 'rxn_6', solver=self.solver))
+        self.assertEqual(fluxes['rxn_1'], 500)
+        self.assertEqual(fluxes['rxn_2'], 0)
+        self.assertEqual(fluxes['rxn_6'], 1000)
+
+if __name__ == '__main__':
+    unittest.main()
