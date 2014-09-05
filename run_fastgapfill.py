a4485821403044573c185c769700af3d58d8271d
Jon Lund Steffensen <jonlst@gmail.com>
2014-09-05 10:09:19 -0400

    run_fastgapfill: Allow setting penalty scores on database reactions
    This uses the fastcore weights parameter to discourage high-penalty
    reactions from being included in the induced model.

--- run_fastgapfill.py
+++ run_fastgapfill.py
@@ -19,6 +19,8 @@ if __name__ == '__main__':
     parser.add_argument('--compounds', metavar='compoundfile', action='append',
                         type=argparse.FileType('r'), default=[],
                         help='Optional compound information table')
+    parser.add_argument('--penalty', metavar='penaltyfile', type=argparse.FileType('r'),
+                        help='List of penalty scores for database reactions')
     parser.add_argument('reactionlist', type=argparse.FileType('r'), help='Model definition')
     parser.add_argument('reaction', help='Reaction to maximize')
     args = parser.parse_args()
@@ -46,15 +48,29 @@ if __name__ == '__main__':
     # Add exchange and transport reactions to database
     model_complete = model.copy()
     print 'Adding database, exchange and transport reactions...'
-    model_complete.add_all_database_reactions(model_compartments)
-    model_complete.add_all_exchange_reactions()
-    model_complete.add_all_transport_reactions()
+    db_added = model_complete.add_all_database_reactions(model_compartments)
+    ex_added = model_complete.add_all_exchange_reactions()
+    tp_added = model_complete.add_all_transport_reactions()
+
+    # Add penalty weights on reactions
+    weights = {}
+    #weights.update((rxnid, 25) for rxnid in db_added)
+    weights.update((rxnid, 50) for rxnid in tp_added)
+    weights.update((rxnid, 250) for rxnid in ex_added)
+
+    if args.penalty:
+        for line in args.penalty:
+            line = line.strip()
+            if len(line) == 0 or line[0] == '#':
+                continue
+            rxnid, weight = line.split(None, 1)
+            weights[rxnid] = float(weight)
 
     # Run Fastcore and print the induced reaction set
     print 'Calculating Fastcore induced set on model...'
     core = set(model.reaction_set)
 
-    induced = fastcore.fastcore(model_complete, core, epsilon)
+    induced = fastcore.fastcore(model_complete, core, epsilon, weights=weights)
     print 'Result: |A| = {}, A = {}'.format(len(induced), induced)
     added_reactions = induced - core
     print 'Extended: |E| = {}, E = {}'.format(len(added_reactions), added_reactions)
@@ -68,10 +84,12 @@ if __name__ == '__main__':
         model_induced.add_reaction(rxnid)
     for rxnid, flux in sorted(flux_balance(model_induced, args.reaction)):
         reaction_class = 'Dbase'
+        weight = weights.get(rxnid, 1)
         if rxnid in model.reaction_set:
             reaction_class = 'Model'
+            weight = 0
         reaction = database.get_reaction(rxnid).translated_compounds(lambda x: compounds.get(x, x))
-        print '{}\t{}\t{}\t{}'.format(rxnid, reaction_class, flux, reaction)
+        print '{}\t{}\t{}\t{}\t{}'.format(rxnid, reaction_class, weight, flux, reaction)
 
     print 'Calculating Fastcc consistent subset of induced model...'
     consistent_core = fastcore.fastcc_consistent_subset(model_induced, epsilon)
