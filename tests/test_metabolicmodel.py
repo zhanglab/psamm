22438b35bf87e329a4823024c6e0d7a95e7cd849
Jon Lund Steffensen <jonlst@gmail.com>
2014-09-10 13:28:36 -0400

    test_metabolicmodel: Extend tests to cover model methods, limits and matrix

--- tests/test_metabolicmodel.py
+++ tests/test_metabolicmodel.py
@@ -31,11 +31,45 @@ class TestMetabolicModel(unittest.TestCase):
         self.database.set_reaction('rxn_6', ModelSEED.parse('|D| =>'))
         self.model = self.database.get_model(self.database.reactions)
 
+    def test_database_property(self):
+        self.assertIs(self.model.database, self.database)
+
     def test_reaction_set(self):
         self.assertEqual(set(self.model.reaction_set), { 'rxn_1', 'rxn_2', 'rxn_3', 'rxn_4', 'rxn_5', 'rxn_6' })
 
     def test_compound_set(self):
         self.assertEqual(set(self.model.compound_set), { ('A', None), ('B', None), ('C', None), ('D', None) })
+
+    def test_add_reaction_new(self):
+        self.database.set_reaction('rxn_7', ModelSEED.parse('|D| => |E|'))
+        self.model.add_reaction('rxn_7')
+        self.assertIn('rxn_7', set(self.model.reaction_set))
+        self.assertIn(('E', None), set(self.model.compound_set))
+
+    def test_add_reaction_existing(self):
+        self.model.add_reaction('rxn_1')
+        self.assertEqual(set(self.model.reaction_set), { 'rxn_1', 'rxn_2', 'rxn_3', 'rxn_4', 'rxn_5', 'rxn_6' })
+        self.assertEqual(set(self.model.compound_set), { ('A', None), ('B', None), ('C', None), ('D', None) })
+
+    def test_add_reaction_invalid(self):
+        with self.assertRaises(Exception):
+            self.model.add_reaction('rxn_7')
+
+    def test_remove_reaction_existing(self):
+        self.model.remove_reaction('rxn_2')
+        self.assertEqual(set(self.model.reaction_set), { 'rxn_1', 'rxn_3', 'rxn_4', 'rxn_5', 'rxn_6' })
+        self.assertEqual(set(self.model.compound_set), { ('A', None), ('C', None), ('D', None) })
+
+    def test_add_all_database_reactions(self):
+        self.database.set_reaction('rxn_7', ModelSEED.parse('|D| => |E|'))
+        added = self.model.add_all_database_reactions()
+        self.assertEqual(added, { 'rxn_7' })
+        self.assertEqual(set(self.model.reaction_set), { 'rxn_1', 'rxn_2', 'rxn_3', 'rxn_4', 'rxn_5', 'rxn_6', 'rxn_7' })
+
+    def test_add_all_database_reactions_none(self):
+        added = self.model.add_all_database_reactions()
+        self.assertEqual(added, set())
+        self.assertEqual(set(self.model.reaction_set), { 'rxn_1', 'rxn_2', 'rxn_3', 'rxn_4', 'rxn_5', 'rxn_6' })
 
     def test_matrix_get_item(self):
         self.assertEqual(self.model.matrix[('A', None), 'rxn_1'], 2)
@@ -46,23 +80,109 @@ class TestMetabolicModel(unittest.TestCase):
         self.assertEqual(self.model.matrix[('C', None), 'rxn_5'], -1)
         self.assertEqual(self.model.matrix[('D', None), 'rxn_5'], 1)
 
-    def test_matrix_iteritems(self):
-        matrix_set = { ((('A', None), 'rxn_1'), 2),
-                         ((('A', None), 'rxn_2'), -1),
-                         ((('B', None), 'rxn_2'), 1),
-                         ((('A', None), 'rxn_3'), -1),
-                         ((('D', None), 'rxn_3'), 1),
-                         ((('A', None), 'rxn_4'), -1),
-                         ((('C', None), 'rxn_4'), 1),
-                         ((('C', None), 'rxn_5'), -1),
-                         ((('D', None), 'rxn_5'), 1),
-                         ((('D', None), 'rxn_6'), -1) }
-        self.assertEqual(set(self.model.matrix.iteritems()), matrix_set)
+    def test_matrix_get_item_invalid_key(self):
+        with self.assertRaises(KeyError):
+            a = self.model.matrix[('A', None), 'rxn_5']
+        with self.assertRaises(KeyError):
+            b = self.model.matrix['rxn_1']
+
+    def test_matrix_set_item_is_invalid(self):
+        with self.assertRaises(TypeError):
+            self.model.matrix[('A', None), 'rxn_1'] = 4
+
+    def test_matrix_iter(self):
+        matrix_keys = { (('A', None), 'rxn_1'),
+                         (('A', None), 'rxn_2'),
+                         (('B', None), 'rxn_2'),
+                         (('A', None), 'rxn_3'),
+                         (('D', None), 'rxn_3'),
+                         (('A', None), 'rxn_4'),
+                         (('C', None), 'rxn_4'),
+                         (('C', None), 'rxn_5'),
+                         (('D', None), 'rxn_5'),
+                         (('D', None), 'rxn_6') }
+        self.assertEqual(set(iter(self.model.matrix)), matrix_keys)
+
+    def test_matrix_len(self):
+        self.assertEqual(len(self.model.matrix), 10)
 
     def test_limits_get_item(self):
         self.assertEqual(self.model.limits['rxn_1'].bounds, (0, 1000))
         self.assertEqual(self.model.limits['rxn_2'].bounds, (-1000, 1000))
         self.assertEqual(self.model.limits['rxn_3'].bounds, (0, 1000))
+
+    def test_limits_get_item_invalid_key(self):
+        with self.assertRaises(KeyError):
+            a = self.model.limits['rxn_7']
+
+    def test_limits_set_item_is_invalid(self):
+        with self.assertRaises(TypeError):
+            self.model.limits['rxn_1'] = None
+
+    def test_limits_set_upper_flux_bounds(self):
+        self.model.limits['rxn_1'].upper = 500
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (0, 500))
+
+    def test_limits_set_upper_flux_bounds_to_invalid(self):
+        with self.assertRaises(ValueError):
+            self.model.limits['rxn_1'].upper = -20
+
+    def test_limits_delete_upper_flux_bounds(self):
+        self.model.limits['rxn_1'].upper = 500
+        del self.model.limits['rxn_1'].upper
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (0, 1000))
+
+    def test_limits_delete_upper_not_set(self):
+        del self.model.limits['rxn_1'].upper
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (0, 1000))
+
+    def test_limits_set_lower_flux_bounds(self):
+        self.model.limits['rxn_1'].lower = 500
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (500, 1000))
+
+    def test_limits_set_lower_flux_bounds_to_invalid(self):
+        with self.assertRaises(ValueError):
+            self.model.limits['rxn_1'].lower = 1001
+
+    def test_limits_delete_lower_flux_bounds(self):
+        self.model.limits['rxn_1'].lower = 500
+        del self.model.limits['rxn_1'].lower
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (0, 1000))
+
+    def test_limits_delete_lower_not_set(self):
+        del self.model.limits['rxn_1'].lower
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (0, 1000))
+
+    def test_limits_set_both_flux_bounds(self):
+        self.model.limits['rxn_2'].bounds = 1001, 1002
+        self.assertEqual(self.model.limits['rxn_2'].lower, 1001)
+        self.assertEqual(self.model.limits['rxn_2'].upper, 1002)
+        self.assertEqual(self.model.limits['rxn_2'].bounds, (1001, 1002))
+
+    def test_limits_set_both_flux_bounds_to_invalid(self):
+        with self.assertRaises(ValueError):
+            self.model.limits['rxn_2'].bounds = 10, -1000
+
+    def test_limits_delete_both_flux_bounds(self):
+        self.model.limits['rxn_2'].bounds = -10, 800
+        del self.model.limits['rxn_2'].bounds
+        self.assertEqual(self.model.limits['rxn_2'].bounds, (-1000, 1000))
+
+    def test_limits_set_both_flux_bounds_delete_lower(self):
+        self.model.limits['rxn_2'].bounds = -10, 800
+        del self.model.limits['rxn_2'].lower
+        self.assertEqual(self.model.limits['rxn_2'].bounds, (-1000, 800))
+
+    def test_limits_set_both_flux_bounds_delete_upper(self):
+        self.model.limits['rxn_2'].bounds = -10, 800
+        del self.model.limits['rxn_2'].upper
+        self.assertEqual(self.model.limits['rxn_2'].bounds, (-10, 1000))
+
+    def test_limits_iter(self):
+        self.assertEqual(set(iter(self.model.limits)), { 'rxn_1', 'rxn_2', 'rxn_3', 'rxn_4', 'rxn_5', 'rxn_6' })
+
+    def test_limits_len(self):
+        self.assertEqual(len(self.model.limits), 6)
 
 class TestMetabolicModelFlipableView(unittest.TestCase):
     def setUp(self):
@@ -102,6 +222,13 @@ class TestMetabolicModelFlipableView(unittest.TestCase):
         self.assertEqual(self.model.limits['rxn_2'].bounds, (-1000, 1000))
         self.assertEqual(self.model.limits['rxn_3'].bounds, (0, 1000))
 
+    def test_flipable_model_view_limits_set_item_after_flip(self):
+        self.model.flip({ 'rxn_1' })
+        self.model.limits['rxn_1'].bounds = -20, 500
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (-20, 500))
+
+        self.model.flip({ 'rxn_1' })
+        self.assertEqual(self.model.limits['rxn_1'].bounds, (-500, 20))
 
 if __name__ == '__main__':
     unittest.main()
