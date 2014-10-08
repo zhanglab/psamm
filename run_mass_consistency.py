9cd0b38c7e319df90c20d979c9993cd62c6f3c94
Jon Lund Steffensen <jonlst@gmail.com>
2014-10-08 13:24:53 -0400

    run_mass_consistency: Allow certain reactions to be excluded from check

--- run_mass_consistency.py
+++ run_mass_consistency.py
@@ -15,6 +15,8 @@ if __name__ == '__main__':
     parser.add_argument('--compounds', metavar='compoundfile', action='append',
                         type=argparse.FileType('r'), default=[],
                         help='Optional compound information table')
+    parser.add_argument('--exclude', metavar='reaction', action='append',
+                        type=str, default=[], help='Exclude reaction from mass consistency')
     parser.add_argument('reactionlist', nargs='?', type=argparse.FileType('r'),
                         help='Model definition')
     args = parser.parse_args()
@@ -44,16 +46,20 @@ if __name__ == '__main__':
         if len(rx.left) == 0 or len(rx.right) == 0:
             exchange.add(reaction_id)
 
+    # Other reactions to exclude from consistency check
+    exclude = set(args.exclude)
+
     # Create set of compounds allowed to have mass zero
     zeromass = set()
     zeromass.add('cpd11632') # Photon
     zeromass.add('cpd12713') # Electron
 
     mass_consistency = MassConsistencyCheck()
+    known_inconsistent = exclude | exchange
 
     print 'Mass consistency on model (database)...'
     epsilon = 1e-5
-    compound_iter = mass_consistency.check_compound_consistency(model, exchange, zeromass)
+    compound_iter = mass_consistency.check_compound_consistency(model, known_inconsistent, zeromass)
 
     print 'Compound consistency...'
     good = 0
@@ -65,10 +71,10 @@ if __name__ == '__main__':
         print '{}: {}'.format(compound.translate(lambda x: compounds.get(x, x)), mass)
     print 'Consistent compounds: {}/{}'.format(good, total)
 
-    print 'Is consistent? {}'.format(mass_consistency.is_consistent(model, exchange, zeromass))
+    print 'Is consistent? {}'.format(mass_consistency.is_consistent(model, known_inconsistent, zeromass))
 
     print 'Reaction consistency...'
-    reaction_iter, compound_iter = mass_consistency.check_reaction_consistency(model, exchange, zeromass)
+    reaction_iter, compound_iter = mass_consistency.check_reaction_consistency(model, known_inconsistent, zeromass)
     for reaction_id, residual in sorted(reaction_iter, key=lambda x: abs(x[1]), reverse=True):
         if abs(residual) >= epsilon:
             reaction = database.get_reaction(reaction_id).translated_compounds(lambda x: compounds.get(x, x))
