90ff16c888b89b364f21d44af66b64c2a0c62020
Jon Lund Steffensen <jonlst@gmail.com>
2014-10-23 10:54:15 -0400

    test_fluxanalysis: Add test for flux_balance_td

--- tests/test_fluxanalysis.py
+++ tests/test_fluxanalysis.py
@@ -43,6 +43,31 @@ class TestFluxBalance(unittest.TestCase):
         self.assertEqual(fluxes['rxn_2'], 0)
         self.assertEqual(fluxes['rxn_6'], 1000)
 
+class TestFluxBalanceThermodynamic(unittest.TestCase):
+    def setUp(self):
+        self.database = DictDatabase()
+        self.database.set_reaction('ex_A', ModelSEED.parse('|A| <=>'))
+        self.database.set_reaction('ex_D', ModelSEED.parse('|D| <=>'))
+        self.database.set_reaction('rxn_1', ModelSEED.parse('|A| => |B|'))
+        self.database.set_reaction('rxn_2', ModelSEED.parse('|B| <=> |C|'))
+        self.database.set_reaction('rxn_3', ModelSEED.parse('|C| <=> |D|'))
+        self.database.set_reaction('rxn_4', ModelSEED.parse('|D| <=> |E|'))
+        self.database.set_reaction('rxn_5', ModelSEED.parse('|E| => |B|'))
+
+        self.model = MetabolicModel.load_model(self.database, self.database.reactions)
+        self.model.limits['ex_A'].lower = -10 # Low uptake
+        self.model.limits['ex_D'].lower = 0 # No uptake
+
+        self.solver = lpsolver.CplexSolver(None)
+
+    def test_flux_balance_td_exchange_d(self):
+        fluxes = dict(fluxanalysis.flux_balance(self.model, 'ex_D', solver=self.solver))
+        self.assertEquals(fluxes['ex_A'], -10)
+        self.assertEquals(fluxes['ex_D'], 10)
+        self.assertEquals(fluxes['rxn_2'], 10)
+        self.assertEquals(fluxes['rxn_4'], 0)
+        self.assertEquals(fluxes['rxn_5'], 0)
+
 class TestNaiveConsistency(unittest.TestCase):
     def setUp(self):
         self.database = DictDatabase()
