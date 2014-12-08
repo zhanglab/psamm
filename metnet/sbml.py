8c10284e234cdc553f1b6d2203d636a89c5f8c23
Jon Lund Steffensen <jonlst@gmail.com>
2014-12-08 11:17:15 -0500

    sbml: Handle the different SBML levels 1-3

--- metnet/sbml.py
+++ metnet/sbml.py
@@ -3,43 +3,78 @@
 
 import xml.etree.ElementTree as ET
 from decimal import Decimal
+from functools import partial
 
 from .database import MetabolicDatabase, DictDatabase
 from .reaction import Reaction, Compound
 
-SBML_NS = 'http://www.sbml.org/sbml/level2'
+# Level 1 namespaces
+SBML_NS_L1 = 'http://www.sbml.org/sbml/level1'
 
-def sbml_name(tag):
-    '''Prepend namespace to SBML tag name'''
-    return '{{{}}}{}'.format(SBML_NS, tag)
+# Level 2 namespaces
+SBML_NS_L2 = 'http://www.sbml.org/sbml/level2'
+SBML_NS_L2_V2 = 'http://www.sbml.org/sbml/level2/version2'
+SBML_NS_L2_V3 = 'http://www.sbml.org/sbml/level2/version3'
+SBML_NS_L2_V4 = 'http://www.sbml.org/sbml/level2/version4'
+SBML_NS_L2_V5 = 'http://www.sbml.org/sbml/level2/version5'
 
-def parse_species_references(root, tag_name):
-    '''Yield species id and parsed value for a speciesReference list'''
-    for species in root.iterfind('./{}/{}'.format(tag_name, sbml_name('speciesReference'))):
-        species_id = species.get('species')
-        species_value = species.get('stoichiometry', 1)
+# Level 3 namespaces
+SBML_NS_L3_V1_CORE = 'http://www.sbml.org/sbml/level3/version1/core'
 
-        value = Decimal(species_value)
-        if value % 1 == 0:
-            value = int(value)
+def tag(tag, namespace=None):
+    '''Prepend namespace to tag name'''
+    if namespace is None:
+        return str(tag)
+    return '{{{}}}{}'.format(namespace, tag)
 
-        yield species_id, value
+class ParseError(Exception):
+    '''Error parsing SBML file'''
 
 class SBMLDatabase(MetabolicDatabase):
+    '''Reaction database backed by an SBML file'''
+
     def __init__(self, file):
         # Parse SBML file
         tree = ET.parse(file)
         root = tree.getroot()
-        self._model = root.find(sbml_name('model'))
 
+        # Parse level and version
+        self._sbml_tag = None
+        self._level = int(root.get('level'))
+        self._version = int(root.get('version'))
+
+        if self._level == 1:
+            self._sbml_tag = partial(tag, namespace=SBML_NS_L1)
+        elif self._level == 2:
+            if self._version == 1:
+                self._sbml_tag = partial(tag, namespace=SBML_NS_L2)
+            elif self._version == 2:
+                self._sbml_tag = partial(tag, namespace=SBML_NS_L2_V2)
+            elif self._version == 3:
+                self._sbml_tag = partial(tag, namespace=SBML_NS_L2_V3)
+            elif self._version == 4:
+                self._sbml_tag = partial(tag, namespace=SBML_NS_L2_V4)
+            elif self._version == 5:
+                self._sbml_tag = partial(tag, namespace=SBML_NS_L2_V5)
+            else:
+                raise ParseError('SBML level 2, version {} not implemented'.format(self._version))
+        elif self._level == 3:
+            if self._version == 1:
+                self._sbml_tag = partial(tag, namespace=SBML_NS_L3_V1_CORE)
+            else:
+                raise ParseError('SBML level 3, version {} not implemented'.format(self._version))
+        else:
+            raise ParseError('SBML level {} not implemented'.format(self._level))
+
+        self._model = root.find(self._sbml_tag('model'))
         self._database = DictDatabase()
 
         # Compounds
         self._model_compounds = {}
-        self._species = self._model.find(sbml_name('listOfSpecies'))
-        for species in self._species.iterfind(sbml_name('species')):
-            species_id = species.get('id')
+        self._species = self._model.find(self._sbml_tag('listOfSpecies'))
+        for species in self._species.iterfind(self._sbml_tag('species')):
             species_name = species.get('name')
+            species_id = species.get('id') if self._level > 1 else species_name
             species_comp = species.get('compartment')
             self._model_compounds[species_id] = species_name, species_comp
 
@@ -51,25 +86,41 @@ class SBMLDatabase(MetabolicDatabase):
                 self._database.set_reaction(reaction_name, reaction)
 
         # Reactions
-        self._reactions = self._model.find(sbml_name('listOfReactions'))
-        for reaction in self._reactions.iterfind(sbml_name('reaction')):
-            reaction_id = reaction.get('id')
+        self._reactions = self._model.find(self._sbml_tag('listOfReactions'))
+        for reaction in self._reactions.iterfind(self._sbml_tag('reaction')):
             reaction_name = reaction.get('name')
+            reaction_id = reaction.get('id') if self._level > 1 else reaction_name
             reaction_rev = reaction.get('reversible', 'true') == 'true'
 
             left = []
-            for species_id, value in parse_species_references(reaction, sbml_name('listOfReactants')):
+            for species_id, value in self._parse_species_references(reaction, 'listOfReactants'):
+                if species_id not in self._model_compounds:
+                    raise ParseError('Reaction {} references non-existent species {}'.format(reaction_id, species_id))
                 species_name, species_comp = self._model_compounds[species_id]
                 left.append((Compound(species_id, compartment=species_comp), value))
 
             right = []
-            for species_id, value in parse_species_references(reaction, sbml_name('listOfProducts')):
+            for species_id, value in self._parse_species_references(reaction, 'listOfProducts'):
+                if species_id not in self._model_compounds:
+                    raise ParseError('Reaction {} references non-existent species {}'.format(reaction_id, species_id))
                 species_name, species_comp = self._model_compounds[species_id]
                 right.append((Compound(species_id, compartment=species_comp), value))
 
             # Add reaction to database
             direction = Reaction.Bidir if reaction_rev else Reaction.Right
             self._database.set_reaction(reaction_id, Reaction(direction, left, right))
+
+    def _parse_species_references(self, root, name):
+        '''Yield species id and parsed value for a speciesReference list'''
+        for species in root.iterfind('./{}/{}'.format(self._sbml_tag(name), self._sbml_tag('speciesReference'))):
+            species_id = species.get('species')
+            species_value = species.get('stoichiometry', 1)
+
+            value = Decimal(species_value)
+            if value % 1 == 0:
+                value = int(value)
+
+            yield species_id, value
 
     @property
     def reactions(self):
@@ -100,12 +151,12 @@ class SBMLDatabase(MetabolicDatabase):
 
     def get_reaction_notes_elements(self):
         '''Yield tuples of reaction ids, and notes as ElementTree elements'''
-        for reaction in self._reactions.iterfind(sbml_name('reaction')):
+        for reaction in self._reactions.iterfind(self._sbml_tag('reaction')):
             reaction_id = reaction.get('id')
-            yield reaction_id, reaction.find(sbml_name('notes'))
+            yield reaction_id, reaction.find(self._sbml_tag('notes'))
 
     def get_compound_notes_elements(self):
         '''Yield tuples of compound ids, and notes as ElementTree elements'''
-        for compound in self._species.iterfind(sbml_name('species')):
+        for compound in self._species.iterfind(self._sbml_tag('species')):
             compound_id = compound.get('id')
-            yield compound_id, compound.find(sbml_name('notes'))
+            yield compound_id, compound.find(self._sbml_tag('notes'))
