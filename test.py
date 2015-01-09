28008fbdb6b60a7fce3de894976060ffb5ba6ed9
Jon Lund Steffensen <jonlst@gmail.com>
2015-01-09 16:26:44 -0500

    Move unit tests into metnet.tests submodule

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import unittest
 
 if __name__ == '__main__':
     # Run unit tests in tests directory
-    testsuite = unittest.TestLoader().discover('tests')
+    testsuite = unittest.TestLoader().discover('metnet/tests')
     unittest.TextTestRunner(verbosity=2).run(testsuite)
