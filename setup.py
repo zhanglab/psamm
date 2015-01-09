27103344ebabbdb90cb17a3beb989cbaa43dc17c
Jon Lund Steffensen <jonlst@gmail.com>
2015-01-09 16:27:48 -0500

    Add initial setuptools-based setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='model_script',
+      version='0.1',
+      description='Metabolic modelling tools',
+      maintainer='Jon Lund Steffensen',
+      maintainer_email='jon_steffensen@uri.edu',
+      url='https://github.com/zhanglab/model_script',
+      packages=find_packages(),
+      scripts=['model.py'],
+      test_suite='metnet.tests')
