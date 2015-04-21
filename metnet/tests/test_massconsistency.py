691413a1642b76e6ec7667052305e39c070dc5a3
Jon Lund Steffensen <jonlst@gmail.com>
2015-04-21 14:38:37 -0400

    Add test cases for massconsistency with zero mass

--- metnet/tests/test_massconsistency.py
+++ metnet/tests/test_massconsistency.py
@@ -6,6 +6,7 @@ from metnet.metabolicmodel import MetabolicModel
 from metnet.database import DictDatabase
 from metnet import massconsistency
 from metnet.datasource.modelseed import parse_reaction
+from metnet.reaction import Compound
 
 try:
     from metnet.lpsolver import cplex
@@ -59,6 +60,43 @@ class TestMassConsistency(unittest.TestCase):
             self.model, exchange=exchange, solver=self.solver)
         for r, residual in reactions:
             self.assertIn(r, self.model.reactions)
+
+
+class TestMassConsistencyZeroMass(unittest.TestCase):
+    """Test mass consistency using a model with zero-mass compound"""
+
+    def setUp(self):
+        # TODO use mock model instead of actual model
+        self.database = DictDatabase()
+        self.database.set_reaction('rxn_1', parse_reaction(
+            '|A| + |B| => |C|'))
+        self.database.set_reaction('rxn_2', parse_reaction(
+            '|C| + |Z| => |A| + |B|'))
+        self.model = MetabolicModel.load_model(
+            self.database, self.database.reactions)
+
+        self.solver = cplex.Solver()
+
+    def test_is_consistent_with_zeromass(self):
+        consistent = massconsistency.is_consistent(
+            self.model, solver=self.solver, zeromass={'Z'})
+        self.assertTrue(consistent)
+
+    def test_compound_consistency_with_zeromass(self):
+        compounds = dict(massconsistency.check_compound_consistency(
+            self.model, solver=self.solver, zeromass={'Z'}))
+        self.assertEquals(compounds[Compound('Z')], 0)
+        for c, value in compounds.iteritems():
+            if c.name != 'Z':
+                self.assertGreaterEqual(value, 1)
+
+    def test_reaction_consistency_with_zeromass(self):
+        reactions, _ = massconsistency.check_reaction_consistency(
+            self.model, solver=self.solver, zeromass={'Z'})
+        reactions = dict(reactions)
+
+        for r, value in reactions.iteritems():
+            self.assertEqual(value, 0)
 
 
 if __name__ == '__main__':
