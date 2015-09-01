faa53ee6cca00d255c0e7ad472be87af08795518
Jon Lund Steffensen <jonlst@gmail.com>
2015-09-01 15:34:11 -0400

    setup.py: Fix missing comma resulting in PyPI upload failure

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4'
     ],
 
