f009e0f5dfb686ae519878684921df2db2c362a3
Jon Lund Steffensen <jonlst@gmail.com>
2015-09-24 13:44:35 -0400

    Add tests for command module

--- psamm/tests/test_command.py
+++ psamm/tests/test_command.py
@@ -15,4 +15,79 @@
 #
 # Copyright 2015  Jon Lund Steffensen <jon_steffensen@uri.edu>
 
-from psamm import command
+import sys
+import os
+import shutil
+import tempfile
+import unittest
+
+from six import StringIO
+
+from psamm.command import main, Command, SolverCommandMixin
+from psamm.lpsolver.generic import RequirementsError
+
+
+class MockCommand(Command):
+    """Test command.
+
+    This is a command for testing basic commands.
+    """
+    init_parser_called = False
+    run_called = False
+    has_native_model = False
+    has_metabolic_model = False
+
+    @classmethod
+    def init_parser(cls, parser):
+        cls.init_parser_called = True
+        parser.add_argument('--test-argument', action='store_true')
+        super(MockCommand, cls).init_parser(parser)
+
+    def __init__(self, *args, **kwargs):
+        super(MockCommand, self).__init__(*args, **kwargs)
+
+    def run(self):
+        self.__class__.run_called = True
+        self.__class__.has_native_model = hasattr(self, '_model')
+        self.__class__.has_metabolic_model = hasattr(self, '_mm')
+
+
+class MockSolverCommand(SolverCommandMixin, Command):
+    """Test solver command.
+
+    This is a command for testing solver commands.
+    """
+    def run(self):
+        solver = self._get_solver()
+
+
+class TestCommandMain(unittest.TestCase):
+    def setUp(self):
+        self._model_dir = tempfile.mkdtemp()
+        with open(os.path.join(self._model_dir, 'model.yaml'), 'w') as f:
+            f.write('''---
+                name: Test model
+                reactions:
+                  - id: rxn_1
+                    equation: '|A| => |B|'
+                compounds:
+                  - id: A
+                  - id: B
+                ''')
+
+    def tearDown(self):
+        shutil.rmtree(self._model_dir)
+
+    def test_command_main(self):
+        main(MockCommand, args=[
+            '--model', self._model_dir, '--test-argument'])
+        self.assertTrue(MockCommand.run_called)
+        self.assertTrue(MockCommand.init_parser_called)
+        self.assertTrue(MockCommand.has_native_model)
+        self.assertTrue(MockCommand.has_metabolic_model)
+
+    def test_solver_command_main(self):
+        with self.assertRaises(RequirementsError):
+            main(MockSolverCommand, args=[
+                '--model', self._model_dir,
+                '--solver', 'name=not-an-actual-solver'])
