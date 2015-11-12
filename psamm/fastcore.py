76fd407bc1610c8a88ecd07f7732beb52c2e0bc3
Jon Lund Steffensen <jonlst@gmail.com>
2015-11-12 13:59:04 -0500

    fastcore: Remove unused support functions

--- psamm/fastcore.py
+++ psamm/fastcore.py
@@ -28,34 +28,6 @@ from .fluxanalysis import FluxBalanceProblem
 logger = logging.getLogger(__name__)
 
 
-def support(fluxiter, threshold=None):
-    """Yield reactions in the support set of the fluxes
-
-    The fluxiter argument must be an iterable returning reaction, flux-pairs.
-    The support set is the set of reactions that have an absolute flux value at
-    or above a certain threshold. If threshold is None the mathematical
-    definition (v != 0) is used.
-    """
-
-    if threshold is None:
-        return (rxnid for rxnid, v in fluxiter if v != 0)
-    return (rxnid for rxnid, v in fluxiter if abs(v) >= threshold)
-
-
-def support_positive(fluxiter, threshold=None):
-    """Yield reactions in the support set of the fluxes (only positive fluxes)
-
-    The fluxiter argument must be an iterable returning reaction, flux-pairs.
-    The support set is the set of reactions that have a flux value at or above
-    a certain threshold. If threshold is None the mathematical definition
-    (v > 0) is used.
-    """
-
-    if threshold is None:
-        return (rxnid for rxnid, v in fluxiter if v > 0)
-    return (rxnid for rxnid, v in fluxiter if v >= threshold)
-
-
 class FastcoreError(Exception):
     """Indicates an error while running Fastcore"""
 
