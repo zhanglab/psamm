b9bda03f5b9674cc7d78be81c15f22dc9cd57a21
Austin Menge <austinmenge@my.uri.edu>
2016-01-25 13:08:13 -0500

    Added ability to select compounds to attempt gapfill with gapfill function

--- psamm/commands/gapfill.py
+++ psamm/commands/gapfill.py
@@ -19,14 +19,24 @@ from __future__ import unicode_literals
 
 import logging
 
-from ..command import Command, MetabolicMixin, SolverCommandMixin
+from ..command import (Command, MetabolicMixin, SolverCommandMixin,
+                       FilePrefixAppendAction)
+
 from ..gapfill import gapfind, gapfill
+
+from psamm.datasource import reaction
 
 logger = logging.getLogger(__name__)
 
 
 class GapFillCommand(MetabolicMixin, SolverCommandMixin, Command):
     """Run the GapFind and GapFill algorithms on the model."""
+    @classmethod
+    def init_parser(cls, parser):
+        parser.add_argument(
+            '--compound', metavar='compound', action=FilePrefixAppendAction,
+            type=str, default=[], help='Select Compounds to GapFill')
+        super(GapFillCommand, cls).init_parser(parser)
 
     def run(self):
         """Run GapFill command"""
@@ -44,26 +54,36 @@ class GapFillCommand(MetabolicMixin, SolverCommandMixin, Command):
 
         solver = self._get_solver(integer=True)
 
-        # Run GapFind on model
-        logger.info('Searching for blocked compounds')
-        blocked = set(compound for compound in gapfind(self._mm, solver=solver)
-                      if compound.compartment != 'e')
-        if len(blocked) > 0:
-            logger.info('Blocked compounds')
-            for compound in blocked:
-                print(compound.translate(lambda x: compound_name.get(x, x)))
+        if len(self._args.compound) == 0:
+            # Run GapFind on model
+            logger.info('Searching for blocked compounds')
+            result = gapfind(self._mm, solver=solver)
+            blocked = set(compound for compound in result
+                          if compound.compartment != 'e')
+            if len(blocked) > 0:
+                logger.info('Blocked compounds')
+                for compound in blocked:
+                    print(compound.translate(
+                          lambda x: compound_name.get(x, x)))
+        else:
+            blocked = set()
+            for c in self._args.compound:
+                cpd_obj = reaction.parse_compound(c)
+                blocked.add(cpd_obj)
+            logger.info('Filling Compounds: {}...'.format(', '.join(str(c)
+                        for c in sorted(blocked))))
 
-        if len(blocked) > 0:
+        if len(blocked):
             # Add exchange and transport reactions to database
             model_complete = self._mm.copy()
             logger.info('Adding database, exchange and transport reactions')
             model_complete.add_all_database_reactions(model_compartments)
             model_complete.add_all_exchange_reactions()
             model_complete.add_all_transport_reactions()
-
-            logger.info('Searching for reactions to fill gaps')
-            added_reactions, reversed_reactions = gapfill(
-                model_complete, core, blocked, solver=solver)
+            if len(blocked) > 0:
+                logger.info('Searching for reactions to fill gaps')
+                added_reactions, reversed_reactions = gapfill(
+                    model_complete, core, blocked, solver=solver)
 
             for rxnid in added_reactions:
                 rx = model_complete.get_reaction(rxnid)
