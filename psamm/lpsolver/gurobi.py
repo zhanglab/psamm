a9afd8a4b6ea658b2154e5eb7cf34252a36dde59
Jon Lund Steffensen <jonlst@gmail.com>
2016-03-17 14:43:38 -0400

    gurobi: Set feasibility tolerance to 1e-9 by default

--- psamm/lpsolver/gurobi.py
+++ psamm/lpsolver/gurobi.py
@@ -80,6 +80,12 @@ class Problem(BaseProblem):
         # using the OutputFlag parameter but instead we lose the log file.
         self._p.params.OutputFlag = 0
 
+        # Set feasibility tolerance. By default, we decrease it to 1e-9.
+        feasibility_tolerance = kwargs.get('feasibility_tolerance', 1e-9)
+        logger.info('Setting feasibility tolerance to {!r}'.format(
+            feasibility_tolerance))
+        self._p.params.FeasibilityTol = feasibility_tolerance
+
         # Set number of threads
         if 'threads' in kwargs:
             logger.info('Setting threads to {!r}'.format(kwargs['threads']))
