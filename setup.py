cd26b56c32bacae44c5ed1b41b6ddec205e9aa34
Jon Lund Steffensen <jonlst@gmail.com>
2016-12-23 16:45:34 -0500

    setup.py: Bump version to 0.27

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as f:
 
 setup(
     name='psamm',
-    version='0.26',
+    version='0.27',
     description='PSAMM metabolic modeling tools',
     maintainer='Jon Lund Steffensen',
     maintainer_email='jon_steffensen@uri.edu',
